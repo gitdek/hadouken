f5893bba0dd229a2948f270008649551768f050c
dek <admin@dek.bz>
2015-01-20 10:26:25 -0500

    Build script to decompress geoip databases.
    Former-commit-id: 0dd24cfe8b779909cfad0d2f3668c987a1491ec6

---
+++ bin/deflategeoip.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Cwd qw(abs_path getcwd);
+use File::Basename;
+
+print "Decompressing GeoIP databases...\n";
+
+#print "argv0 is $ARGV[0]\n";
+
+my $script_abs = abs_path($0);
+my $script_dir = dirname($script_abs);
+# print "script dir is $script_dir\n";
+
+my $cwd = getcwd();
+#print "current dir is $cwd\n";
+
+chdir($script_dir) or die("Failed to change to directory: $!"); 
+chdir "../data/geoip/" or die("Failed to change to directory: $!");
+
+$cwd = getcwd();
+# print "current dir is $cwd\n";
+system("gzip -dq *.gz");
+
+print "Finished decompressing.\n";
+#my $geoip_gzip = "$path../data../geoip/";
+
+exit 0;
+
